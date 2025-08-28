b49c7c24798cc17e8ede3d24aff2b7d54c23a6ee
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-08-28 14:29:16 -0300

    Add solution - CodeHub

---
+++ 7-kyu/find-the-divisors/find-the-divisors.py
@@ -0,0 +1,6 @@
+def divisors(integer):
+    result=[]
+    for i in range(2,integer):
+        if integer%i == 0:
+            result.append(i)
+    return result if result else str(integer) + ' is prime'
