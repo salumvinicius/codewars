a7f9ff30e1e5516101452fec211bd897fba8f4c8
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-09-05 10:52:55 -0300

    Update solution - CodeHub

--- 6-kyu/your-order-please/your-order-please.py
+++ 6-kyu/your-order-please/your-order-please.py
@@ -1,10 +1,11 @@
 def order(sentence):
-    sentence = sentence.split()
-    resultado = [''] * len(sentence)
-    for i in sentence:
-        for x in i:
-            if x.isdigit():
-                indice = int(x) - 1
-                resultado[indice] = i
-    
-    return ' '.join(resultado)
+    lista = []
+    for palavra in sentence.split():
+        for letra in palavra:
+            if letra.isdigit():
+                lista.append(letra + palavra)
+    lista = sorted(lista)
+    resultado = []
+    for palavra_1 in lista:
+        resultado.append(palavra_1[1:])
+    return " ".join(resultado)
