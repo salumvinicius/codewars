56fad7fff20dd94c0a0f1611b30726f2c2528614
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-09-19 16:04:49 -0300

    Update solution - CodeHub

--- 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.py
+++ 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.py
@@ -1,7 +1,7 @@
 def solution(number):
-    soma = 0
-    for i in range(number):
-        if i % 3 == 0 or i % 5 == 0:
-            soma += i
-    return soma
+    res = 0
+    for number in range(number):
+        if number % 3 == 0 or number % 5 == 0:
+            res += number
+    return res
   
