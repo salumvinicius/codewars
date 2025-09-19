876feae53d6112f8500f95150f78669daa077b18
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-09-19 15:48:30 -0300

    Add solution - CodeHub

---
+++ 7-kyu/small-enough-beginner/small-enough-beginner.py
@@ -0,0 +1,10 @@
+ 
+def small_enough(array, limit):
+    res = []
+    for numero in array:
+        if numero > limit:
+            return False
+        else:
+            res.append(numero)
+    if res == array:
+        return True
