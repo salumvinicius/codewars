ca070e773c18299dfd75e47056676d76ad402a0d
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-09-19 11:14:09 -0300

    Update solution - CodeHub

--- 6-kyu/write-number-in-expanded-form/write-number-in-expanded-form.py
+++ 6-kyu/write-number-in-expanded-form/write-number-in-expanded-form.py
@@ -1,7 +1,8 @@
+ 
 def expanded_form(num):
     res = []
     for i , numero in enumerate(str(num)):
         if numero != 0:
-            res.append(numero + '0' * len(str(num) - 1))
+            res.append(numero + '0' * len(str(num - 1)))
     
     return res
