43bc33dcda3e635efc4b2649006c3d78ea35061b
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-09-19 11:02:21 -0300

    Update solution - CodeHub

--- 8-kyu/welcome/welcome.py
+++ 8-kyu/welcome/welcome.py
@@ -1,4 +1,3 @@
- 
 def greet(language):
     db = {"english" : "Welcome", "czech" : "Vitejte" , "danish": "Velkomst" , "dutch": "Welkom" , "estonian": "Tere tulemast", "finnish": "Tervetuloa", "flemish": "Welgekomen", "french": "Bienvenue", "german": "Willkommen", "irish": "Failte", "italian": "Benvenuto", "latvian": "Gaidits", "lithuanian": "Laukiamas", "polish": "Witamy", "spanish": "Bienvenido", "swedish": "Valkommen", "welsh": "Croeso"}
     try:
