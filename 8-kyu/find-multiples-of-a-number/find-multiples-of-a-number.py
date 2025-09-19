68a0c602055373ab29295b60cb0c280a583c29d8
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-09-19 11:04:20 -0300

    Update solution - CodeHub

--- 8-kyu/find-multiples-of-a-number/find-multiples-of-a-number.py
+++ 8-kyu/find-multiples-of-a-number/find-multiples-of-a-number.py
@@ -1,7 +1,7 @@
  
 def find_multiples(integer, limit):
     res = []
-    for num in range(integer , limit + 1, 5):
+    for num in range(integer , limit + 1, integer):
         res.append(num)
     
     return res
