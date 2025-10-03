654fafc4c059d8ad3e1b8cb8f8d455e535cbd005
Vinicius Salum <50721619+salumvinicius@users.noreply.github.com>
2025-10-03 16:24:37 -0300

    Add solution - CodeHub

---
+++ 7-kyu/make-a-function-that-does-arithmetic/make-a-function-that-does-arithmetic.py
@@ -0,0 +1,10 @@
+ 
+def arithmetic(a, b, operator):
+    if operator == "add":
+        return a+b
+    if operator == "subtract":
+        return a-b
+    if oprator == "multiply":
+        return a*b
+    else:
+        return a/b
